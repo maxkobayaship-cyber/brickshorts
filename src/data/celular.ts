4ce7ce5054a854e62ed5d30bebceb4a1f2de678f
maxkobayaship-cyber <maxkobayaship@gmail.com>
2026-09-06 13:20:26 -0700

    Split catalogs into part files

---
+++ src/data/celular.ts
@@ -0,0 +1,5 @@
+import { CELULAR_SHORTS_A } from "@/data/celular-a";
+import { CELULAR_SHORTS_B } from "@/data/celular-b";
+import type { BrickShort } from "@/types/short";
+
+export const CELULAR_SHORTS: BrickShort[] = [...CELULAR_SHORTS_A, ...CELULAR_SHORTS_B];
---
+++ src/data/lego.ts
@@ -0,0 +1,5 @@
+import { LEGO_SHORTS_A } from "@/data/lego-a";
+import { LEGO_SHORTS_B } from "@/data/lego-b";
+import type { BrickShort } from "@/types/short";
+
+export const LEGO_SHORTS: BrickShort[] = [...LEGO_SHORTS_A, ...LEGO_SHORTS_B];
